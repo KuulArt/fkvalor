4058f8f8478d57e717b6d9c2f9e31c140cbbbe01
Toms Lisments <toms.lisments@live.com>
2017-12-03 19:13:15 +0200

    Landing Page initial commit

---
+++ static/bundle.js
@@ -0,0 +1,115 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+// function getTimeRemaining(endtime) {
+//     var t = Date.parse(endtime) - Date.parse(new Date());
+//     var seconds = Math.floor((t / 1000) % 60);
+//     var minutes = Math.floor((t / 1000 / 60) % 60);
+//     var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+//     var days = Math.floor(t / (1000 * 60 * 60 * 24));
+//     return {
+//       'total': t,
+//       'days': days,
+//       'hours': hours,
+//       'minutes': minutes,
+//       'seconds': seconds
+//     };
+//   }
+
+//   function initializeClock(id, endtime) {
+//     var clock = document.getElementById(id);
+//     var daysSpan = clock.querySelector('.days');
+//     var hoursSpan = clock.querySelector('.hours');
+//     var minutesSpan = clock.querySelector('.minutes');
+//     var secondsSpan = clock.querySelector('.seconds');
+
+//     function updateClock() {
+//       var t = getTimeRemaining(endtime);
+
+//       daysSpan.innerHTML = t.days;
+//       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+//       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+//       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+//       if (t.total <= 0) {
+//         clearInterval(timeinterval);
+//       }
+//     }
+
+//     updateClock();
+//     var timeinterval = setInterval(updateClock, 1000);
+//   }
+
+//   const deadline = new Date(2018, 1, 1);;
+//   initializeClock('clockdiv', deadline);
+
+
+/***/ })
+/******/ ]);
